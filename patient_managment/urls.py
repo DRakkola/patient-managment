bb00a76690ad921cca2ea54c9f55fe004ce6ccba
debbichi raki <debbichi1997@gmail.com>
2021-05-04 03:07:25 +0200

    cleaner

--- patient_managment/urls.py
+++ patient_managment/urls.py
@@ -20,13 +20,16 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name="index"),
-    path('patients', views.patients, name="patients"),
-    path('dashboard', views.dashboard, name="dashboard"),
-    path('accounts/profile', views.ProfileView.as_view(), name="profile"),
-
+    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
+    path("patients", views.patients, name="patients"),
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("accounts/profile", views.ProfileView.as_view(), name="profile"),
     # Django Auth
-    path('accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
-    path('accounts/logout', auth_views.LogoutView.as_view(), name="logout")
+    path(
+        "accounts/login",
+        auth_views.LoginView.as_view(template_name="accounts/login.html"),
+        name="login",
+    ),
+    path("accounts/logout", auth_views.LogoutView.as_view(), name="logout"),
 ]
--- patient_managment/views.py
+++ patient_managment/views.py
@@ -1,22 +1,24 @@
-#from django.http import HttpResponse
-#from django.template import loader
+# from django.http import HttpResponse
+# from django.template import loader
 from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-def index(request):
-    return render(request, 'index.html')
-    #template = loader.get_template('index.html')
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "index.html")
+    # template = loader.get_template('index.html')
     # return HttpResponse(template.render({}, request))
 
 
-def patients(request):
-    return render(request, 'patients.html')
+def patients(request: HttpRequest) -> HttpResponse:
+    return render(request, "patients.html")
 
 
-def dashboard(request):
-    return render(request, 'dashboard.html')
+def dashboard(request: HttpRequest) -> HttpResponse:
+    return render(request, "dashboard.html")
+
 
 class ProfileView(LoginRequiredMixin, TemplateView):
-    template_name = 'accounts/profile.html'
+    template_name = "accounts/profile.html"
